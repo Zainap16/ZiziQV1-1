ea0ea141489ef79ef44d263e6cf9d562e621f258
Zainap16 <zvanblerck1@gmail.com>
2025-03-16 21:43:32 +0200

    s

--- scenes/scripts/zombie_actions.gd
+++ scenes/scripts/zombie_actions.gd
@@ -2,13 +2,19 @@ extends CharacterBody3D
 
 var player = null
 const SPEED = 4.0
+const ATTACK_RANGE = 2.5
+
+
 @export var player_path : NodePath
+@onready var animation_tree = $AnimationTree
 
 @onready var navigation_agent = $NavigationAgent3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_node(player_path)
+	
+
  
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,6 +24,14 @@ func _process(delta):
 	var next_nav_agent = navigation_agent.get_next_path_position()
 	velocity = (next_nav_agent - global_transform.origin).normalized() * SPEED
 	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP, true)
+	
+#	condition
+	animation_tree.set("parameters/conditions/attack",_target_in_range())
+	animation_tree.set("parameters/conditions/run", !_target_in_range())
+	
+	#animation_tree.get("parameters/playback")
+	
 	move_and_slide()
 
-	
+func  _target_in_range():
+	return global_position.distance_to(player.global_position) < ATTACK_RANGE
